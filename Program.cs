378229a74d875b729b5d286010eef610422477ef
TatikondaAnvitha123 <tatikondaanvitha05@gmail.com>
2024-02-25 12:29:00 +0530

    Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Joe's Pizza Console App!");
+        Console.WriteLine("This is a placeholder for the web application.");
+        Console.ReadLine(); 
+    }
+}
